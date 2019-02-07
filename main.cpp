4b112f56dbeb558a27f81f12869a59dd9bf5ab7a
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-02-06 18:41:14 -0600

    Adding our event loop

--- main.cpp
+++ main.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 using namespace std;
+
+int main(){
+	
+	char op = 'A'; 
+	char again = 'Y';
+	
+	cout << "Our C++ Calculator\n";
+	
+while('n' != again){
+	cout << "(A)add or (S)subtract?";
+	cin >> op;
+	cout << " Do you want to use Calculator again (Y/n)?";
+	cin >> again;
+	}
+	
+	return 0;
+}
