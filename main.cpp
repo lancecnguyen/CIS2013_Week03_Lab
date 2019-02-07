c6a3099aa1d620462332121d1f438c495a87ab81
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-02-06 19:59:48 -0600

    Using Void and Switch

--- main.cpp
+++ main.cpp
@@ -1,36 +1,46 @@
 #include <iostream>
 using namespace std;
-
-int main(){
-	
-	char op = 'A'; 
-	char again = 'Y';
 	int num1 = 0;
 	int num2 = 0;
-	
-	cout << "Our C++ Calculator\n";
-	
-while('n' != again){
-	cout << "(A)add or (S)subtract or (M)multiply?";
-	cin >> op;
-	
+
+void printoptions(){
 	cout << "Enter first number: ";
 	cin >> num1;
 	cout << "Enter second number: ";
 	cin >> num2;
+}
+int main(){
 	
-	if(('A' == op) || ('a' == op)){
-		cout << num1 << " + " << num2 << " = " << (num1 + num2) << endl;
-	} else if(('S' == op) || ('s' == op)){
-		cout << num1 << " - " << num2 << " = " << (num1 - num2) << endl;
-	} else if(('M' == op) || ('m' == op)){
-		cout << num1 << " * " << num2 << " = " << (num1 * num2) << endl;
-	} else {
-		cout << op << " is not a valid option..." << endl;
+	char op = 'A'; 
+	char again = 'Y';
+
+	
+	cout << "Our C++ Calculator\n";
+	
+	do{
+		cout << "(A)add or (S)subtract?";
+		cin >> op;
+		
+		switch ( op ){
+			case 'a':
+			case 'A':
+			printoptions();
+				cout << endl << num1 << " + "
+					<< num2 << " = " << ( num1 + num2) << endl;
+					break;
+			case 's':
+			case 'S':
+				printoptions();
+				cout << endl << num1 << " - "
+					<< num2 << " = " << ( num1 - num2) << endl;
+					break;
+			default:
+				cout << op << " is not a valid option..." << endl;
 		}
-	cout << endl << "Do you want to use Calculator again (Y/n)?";
-	cin >> again;
-	}
+			
+		cout << endl << "Do you want to use Calculator again (Y/n)?";
+		cin >> again;
+	} while ('n' != again);
 	
 	return 0;
 }
