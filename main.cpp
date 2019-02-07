041b8eccae38eb35e787d3c3d493a6c100e8924e
BT228 <svc_OKC_BT228@ad.osuokc.edu>
2019-02-06 19:09:55 -0600

    Addition calculator

--- main.cpp
+++ main.cpp
@@ -5,13 +5,23 @@ int main(){
 	
 	char op = 'A'; 
 	char again = 'Y';
+	int num1 = 0;
+	int num2 = 0;
 	
 	cout << "Our C++ Calculator\n";
 	
 while('n' != again){
 	cout << "(A)add or (S)subtract?";
 	cin >> op;
-	cout << " Do you want to use Calculator again (Y/n)?";
+	
+	cout << "Enter first number to add: ";
+	cin >> num1;
+	cout << "Enter second number to add: ";
+	cin >> num2;
+	
+	cout << num1 << " + " << num2 << " = " << (num1 + num2) << endl;
+	
+	cout << endl << "Do you want to use Calculator again (Y/n)?";
 	cin >> again;
 	}
 	
